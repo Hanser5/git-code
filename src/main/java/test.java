5d32ea33b8b3c733b3663ab511664b971d118681
Saber <Saber@atguigu.com>
2021-09-13 20:46:42 +0800

    2

---
+++ src/main/java/test.java
@@ -0,0 +1,17 @@
+/**
+ * @auther xuexx
+ * @create 2021-09-13 20:45
+ */
+
+/**
+ *@ClassName test
+ *@Description TODO
+ *@Author xuexx
+ *@Date 2021/9/13 20:45
+ *@Version 1.0
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
