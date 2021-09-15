0c263a178767612e2f48c66e1bf1bf8876007d02
Saber <Saber@atguigu.com>
2021-09-15 21:17:56 +0800

    master 3

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -16,5 +16,6 @@ public class test {
         System.out.println("1234");
         System.out.println("master 1234");
         System.out.println("hot-fix");
+        System.out.println("hot-fix 2");
     }
 }
